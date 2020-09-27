3f1d26a8d10f62e826f8f7cba5b905489dd64116
Marcelo Crivelli <crivellimarcelo@gmail.com>
2020-09-27 20:54:57 -0300

    Creación entities: Operation, Operation_Type, Operation_Parameter, Convertion

---
+++ src/main/java/com/proyecto/flowmanagement/backend/persistence/entity/Convertion.java
@@ -0,0 +1,65 @@
+package com.proyecto.flowmanagement.backend.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "convertion")
+public class Convertion {
+
+    @Column(name = "condition")
+    private String condition;
+
+    @Column(name = "source_unit")
+    private String sourceUnit;
+
+    @Column(name = "destination_unit")
+    private String destinationUnit;
+
+    @ManyToOne
+    @JoinColumn(name = "operation_parameter_id")
+    private Operation_Parameter operationParameter;
+
+//    @ManyToOne
+//    @JoinColumn(name = "convertion_type_id")
+//    private Convertion_Type ConvertionType;
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getSourceUnit() {
+        return sourceUnit;
+    }
+
+    public void setSourceUnit(String sourceUnit) {
+        this.sourceUnit = sourceUnit;
+    }
+
+    public String getDestinationUnit() {
+        return destinationUnit;
+    }
+
+    public void setDestinationUnit(String destinationUnit) {
+        this.destinationUnit = destinationUnit;
+    }
+
+    public Operation_Parameter getOperationParameter() {
+        return operationParameter;
+    }
+
+    public void setOperationParameter(Operation_Parameter operationParameter) {
+        this.operationParameter = operationParameter;
+    }
+
+//    public Convertion_Type getConvertionType() {
+//        return ConvertionType;
+//    }
+//
+//    public void setConvertionType(Convertion_Type convertionType) {
+//        ConvertionType = convertionType;
+//    }
+}
---
+++ src/main/java/com/proyecto/flowmanagement/backend/persistence/entity/Operation_Type.java
@@ -0,0 +1,33 @@
+package com.proyecto.flowmanagement.backend.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "operation_type")
+public class Operation_Type {
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "label")
+    private String label;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+}
